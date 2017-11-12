a18d8188a293c32656aa5621a3aef0e564e1b31b
Okeke Abuchi <okekeag@gmail.com>
2017-11-12 15:51:20 -0800

    Initial commit

---
+++ app/src/main/java/com/buchi/cryptoX/api/APIService.java
@@ -0,0 +1,16 @@
+package com.buchi.cryptoX.api;
+
+
+import com.buchi.cryptoX.models.CryptoCompare;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+
+public interface APIService {
+
+    //getting latest currency exchange rates
+    @GET("pricemulti?fsyms=ETH,BTC&tsyms=NGN,KES,USD,GBP,CNY,CHF,EUR,MXN,GLD,JOD,OMR,BHD,KWD,JPY,CAD,AED,CLP,BRL,INR,ILS")
+    Call<CryptoCompare> getLatestExchangeRates();
+
+}
---
+++ app/src/main/java/com/buchi/cryptoX/api/APIUrl.java
@@ -0,0 +1,5 @@
+package com.buchi.cryptoX.api;
+public class APIUrl {
+    //Base URL for CryptoCompare API
+    public static final String BASE_URL = "https://min-api.cryptocompare.com/data/";
+}
